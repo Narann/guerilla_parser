8bc808e5080ba95f4a04810ae90f232582f265ad
Dorian Fevrier <fevrier.dorian@yahoo.fr>
2017-10-13 16:56:08 +0200

    improve setup system

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+import os.path
+import sys
 from setuptools import setup
 
+_repo_path = os.path.split(__file__)[0]
+sys.path.append(os.path.join(_repo_path, 'src'))
+
+import guerilla_parser
+
 setup(name='guerilla_parser',
-      version='0.1.0',
-      scripts=['helloworld'],
+      version=guerilla_parser.__version__,
       license='MIT',
       author='Dorian Fevrier',
       url='https://github.com/Narann/guerilla_parser',
@@ -10,6 +16,8 @@ setup(name='guerilla_parser',
       description=('This python module provide an easy way to parse Guerilla '
                    'files (only .gproject files for now) and navigate into '
                    'parsed nodes and plugs.'),
+      long_description=open('README.md').read(),
+      include_package_data=True,  # enable MANIFEST.in
       keywords='guerilla, parser, gproject',
       classifiers=[
           'Development Status :: 3 - Alpha',
